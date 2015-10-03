b7bf919142091432da92c843098d2508623d1848
David Siver <dsiver1@my.westga.edu>
2015-10-03 15:40:57 -0400

    changed intervals and serial prints

--- volume-meter.ino
+++ volume-meter.ino
@@ -11,13 +11,13 @@
 #include <SPI.h>
 #include <TFT.h>
 
-#define DEBUG
+#define DEBUG 0
 #define serialBaudRate 9600
 #define minAmplitude 0
 #define maxAmplitude 1023
-#define rectangleWidthInterval EsploraTFT.width()/3
-#define yellowRectangleStart EsploraTFT.width()-(rectangleWidthInterval*2)
-#define redRectangleStart EsploraTFT.width()-rectangleWidthInterval
+#define rectangleWidthInterval EsploraTFT.width()/6
+#define yellowRectangleStart 0+rectangleWidthInterval
+#define redRectangleStart 0+(rectangleWidthInterval*3)
 #define rectangleHeight 10
 
 int oldRectangleWidth = 0;
@@ -26,6 +26,15 @@ void setup() {
   EsploraTFT.begin();
   Serial.begin(serialBaudRate);
   EsploraTFT.background(0, 0, 0);
+#ifdef DEBUG
+  //  char text[4];
+  //  String(EsploraTFT.width()).toCharArray(text, 4);
+  //  EsploraTFT.stroke(255, 255, 255);
+  //  EsploraTFT.text(text, 0, 0);
+  Serial.print("yellowRectangleStart: " + String(yellowRectangleStart));
+  Serial.print(" redRectangleStart: " + String(redRectangleStart));
+  Serial.println("");
+#endif
 }
 
 void loop() {
@@ -34,7 +43,6 @@ void loop() {
   drawGraph(loudness);
 }
 
-// EsploraTFT.rect(xStart, yStart, width, height);
 void drawGraph(int loudness) {
   int newRectangleWidth = map(loudness, minAmplitude, maxAmplitude, 0, EsploraTFT.width());
   newRectangleWidth = constrain(newRectangleWidth, 0, EsploraTFT.width());
@@ -44,38 +52,35 @@ void drawGraph(int loudness) {
   }
   drawRectangle(newRectangleWidth);
   oldRectangleWidth = newRectangleWidth;
-#ifdef DEBUG
-  outputToSerialMonitor(loudness, newRectangleWidth);
-#endif
 }
 
 void drawRectangle(int width) {
+  EsploraTFT.fill(0, 0, 0);
+  EsploraTFT.rect(0, EsploraTFT.height() / 2, EsploraTFT.width(), rectangleHeight);
   if (width < yellowRectangleStart) {
     EsploraTFT.fill(0, 255, 0);
     EsploraTFT.rect(0, EsploraTFT.height() / 2, width, rectangleHeight);
   }
-  else if (width < redRectangleStart && width > yellowRectangleStart) {
+  else if (width > yellowRectangleStart && width < redRectangleStart) {
     EsploraTFT.fill(0, 255, 0);
     EsploraTFT.rect(0, EsploraTFT.height() / 2, yellowRectangleStart, rectangleHeight);
     EsploraTFT.fill(0, 255, 255);
+    width -= yellowRectangleStart;
     EsploraTFT.rect(yellowRectangleStart, EsploraTFT.height() / 2, width, rectangleHeight);
+#ifdef DEBUG
+    Serial.println("Y, width: " + String(width));
+#endif
   }
-  else if (width < EsploraTFT.width() && width > redRectangleStart) {
+  else if (width > redRectangleStart) {
     EsploraTFT.fill(0, 255, 0);
     EsploraTFT.rect(0, EsploraTFT.height() / 2, yellowRectangleStart, rectangleHeight);
     EsploraTFT.fill(0, 255, 255);
     EsploraTFT.rect(yellowRectangleStart, EsploraTFT.height() / 2, redRectangleStart, rectangleHeight);
     EsploraTFT.fill(0, 0, 255);
     EsploraTFT.rect(redRectangleStart, EsploraTFT.height() / 2, width, rectangleHeight);
+#ifdef DEBUG
+    Serial.println("R, width: " + String(width));
+#endif
   }
-  delay(100);
-  EsploraTFT.fill(0, 0, 0);
-  EsploraTFT.rect(0, EsploraTFT.height() / 2, EsploraTFT.width(), rectangleHeight);
-}
-
-void outputToSerialMonitor(int loudness, int mappedValue) {
-  Serial.print("loudness: " + String(loudness));
-  Serial.print(" newRectangleWidth: " + String(mappedValue));
-  Serial.print(" oldRectangleWidth: " + String(oldRectangleWidth));
-  Serial.println("");
+  delay(10);
 }
